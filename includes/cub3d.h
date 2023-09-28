e35b016b3cd85ca552b844701ca83954b0ea4a0c
ydimitro <99765026+ydimitro@users.noreply.github.com>
2023-09-28 10:43:36 +0200

    Add files via upload

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -22,7 +22,10 @@
 # include "../libft/includes/libft.h"
 # include "../minilibx-linux/mlx.h"
 
+// Maximum map dimensions
 #define MAP_MAX_SIZE 1024
+#define MAX_HEIGHT 500
+#define MAX_WIDTH 500
 
 #define MAX_MAP_LINES 100
 
@@ -33,8 +36,12 @@
 #define ERR_INVALID_EXT 4
 #define ERR_READ_FILE 5
 #define ERR_PLAYER_NR 6
+#define ERR_MAP_ALLOCATION_FAILED 7
+#define ERR_TEXTURE_LOAD_FAILED 8
+#define ERR_EMPTY_MAP 9
 
 
+#define SCREEN_WIDTH 800
 #define FOV (60 * (M_PI / 180))
 #define DIST_TO_PROJ_PLANE (SCREEN_WIDTH / 2) / tan(FOV / 2)
 #define MOVE_SPEED 60
@@ -80,6 +87,8 @@ typedef struct s_texture_calc
     double draw_start;
     double tex_pos;
     int tex_x;
+	int tex_y;
+    int color;
 } t_texture_calc;
 
 typedef struct s_texture {
@@ -105,6 +114,16 @@ typedef struct s_player {
 	double speed;
 	double turn_speed;
 }	t_player;
+
+//drawing.c
+typedef struct s_line_params
+{
+    int dx;
+    int dy;
+    int sx;
+    int sy;
+    int err;
+}               t_line_params;
 
 typedef struct	s_line
 {
@@ -155,41 +174,80 @@ typedef struct s_data {
 	t_texture west_tex;
 }	t_data;
 
+//destroy.c
+void	cleanup_texture(t_data *data);
+int	free_mem(t_data *data);
 
-int manage_fd(char *filename, t_data *data);
-int	is_map_valid(t_data *data);
-int free_mem(t_data *data);
-void data_initiziated(t_data *data);
-int get_map(char *line, t_data *data);
+//direction.c
+int	set_player_start(t_data *data, char orientation, int x, int y);
 
-void cleanup_texture(t_data *data);
-int free_mem(t_data *data);// Frees any dynamically allocated memory associated with the t_data structure
-int set_player_start(t_data *data, char orientation, int x, int y);
+//drawing_utils_2.c
+int		custom_abs(int x);
+void	calc_line_dir(t_data *data, t_line *line);
+void	calc_wall_x(t_data *data, t_texture_calc *texture_calc);
+void	calc_deltas_and_steps(t_line *line, t_line_params *params);
+int		get_wall_color(t_data *data);
+
+//drawing_utils_texture.c
+void	set_texture(t_data *data, t_texture **texture);
+void	calc_texture_pos(t_data *data, t_texture_calc *texture_calc);
+void	initialize_texture_variables(t_data *data, t_texture_calc *texture_calc);
+void	draw_textured_walls(t_data *data, int x, t_texture *texture);
+void	draw_wall_slice(t_data *data, int x);
+
+//drawing.c
+void	draw_circle(t_data *data, int x, int y);
+void	set_line_endpoints(t_data *data, t_line *line);
+void	draw_player(t_data *data, t_line *line);
+void	draw_line(t_data *data, t_line *line);
+
+//error_handling.c
 void handle_error(int code);
-void data_init(t_data *data);
-int key_press(int keycode, t_data *data);
-int manage_fd(char *filename, t_data *data); //opening and reading the game map from a file
-void move_player(t_data *data, float dx, float dy);
-int textures_files(t_data *data);
-int textures_info(t_data *data);
-int	is_map_valid(t_data *data); // Validates if the given map is correct or valid to be played
-void draw_wall_slice(t_data *data, int x);
-void cast_through_map(t_data *data);
-void cast_single_ray(t_data *data, int x);
-int ft_isspace(int c);
+
+//init.c
+void	data_init(t_data *data);
+void	load_all_textures(t_data *data);
+
+//map_check.c
+int	is_map_empty(t_data *data);
+int	check_valid_chars(t_data *data);
+int	check_player_count(char **map, int height, int width);
+bool	is_map_closed(t_data *data);
+int	check_boundary_cells(bool visited[][MAX_WIDTH], int height, int width);
+
+//map_get_valid.c
+int	ft_isspace(int c);
+int	get_map(char *line, t_data *data);
+int	is_map_valid(t_data *data);
+
+//map_parse_utils.c
+int	is_valid_char(char c);
+int	count_players(char **map, int height, int width);
+void	dfs(int row, int col, t_data *data,
+	bool visited[data->map_height][data->map_width]);
+int	resize_game_map(t_data *data);
+int	allocate_initial_map(t_data *data);
+
+//movement.c
+void	handle_movement(int keycode, t_data *data, float *dx, float *dy);
+void	handle_rotation(int keycode, t_data *data);
+int		key_press(int keycode, t_data *data);
+void	move_player(t_data *data, float dx, float dy);
+
+
+///////////////////////////////////////////////////////////////////////////////////
+//raycasting.c
+void	set_wall_distance(t_data *data);
+void	cast_through_map(t_data *data);
+void	cast_single_ray(t_data *data, int x);
+void	set_ray_step_and_side_dist(t_data *data);
+
+//rendering.c
+void render(t_data *data);
+
+//textures_parse.c
+int get_elements( char *line, t_data *data);
 int	parse_texture(char *line, t_data *data);
 int	parse_color(char *line, t_data *data);
-void load_texture(t_data *data, t_texture *tex, char *path);
-void render(t_data *data);
-int	get_elements(char *line, t_data *data);
-int		get_elements(char *line, t_data *data);
-void	draw_circle(t_data *data, int x, int y);
-void	draw_player(t_data *data, t_line *line);
-void draw_textured_walls(t_data *data, int x, t_texture *tex);
-void	draw_line(t_data *data, t_line *line);
-void	set_texture(t_data *data, t_texture **tex);
-void calculate_wall_x(t_data *data, t_texture_calc *tex_calc);
-void calculate_texture_pos(t_data *data, t_texture_calc *tex_calc);
-void	calculate_line_dir(t_data *data, t_line *line);
-//void draw_rectangle(t_data *data, int x, int y, int width, int height, int color);
+
 #endif
